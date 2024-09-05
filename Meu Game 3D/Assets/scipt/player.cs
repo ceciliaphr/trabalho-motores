7ae2ce6f0b714769f3e704bbdc4c9f81ef1a72ed
ceciliaphr <cecilia.pinheiro@escolar.ifrn.edu.br>
2024-09-05 11:08:44 -0300

    Estrela

--- Meu Game 3D/Assets/scipt/player.cs
+++ Meu Game 3D/Assets/scipt/player.cs
@@ -6,32 +6,41 @@ public class Player : MonoBehaviour
 {
     public int velocidade = 10;
     public int forcaPulo = 7;
-   private Rigidbody rb;
+    public bool noChão;
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-       Debug.Log(message: "START");
-        TryGetComponent(out rb);
+       Debug.Log(message:"START");
+       TryGetComponent(out rb);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!noChão && collision.gameObject.tag == "Chão")
+        {
+            noChão = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-         Debug.Log(message: "UPDATE");
+         Debug.Log("UPDATE");
          float h = Input.GetAxis("Horizontal"); //-1 esquerda, 0 nada, 1 direita
-        float v = Input.GetAxis("Vertical"); //-1 pra tras, 0 nada, 1 pra frente
+         float v = Input.GetAxis("Vertical"); //-1 pra tras, 0 nada, 1 pra frente
 
 
              UnityEngine.Vector3 direcao = new Vector3(h, 0, v);
-            rb.AddForce(direcao * velocidade * Time.deltaTime,ForceMode.Impulse);
+             rb.AddForce(direcao * velocidade * Time.deltaTime,ForceMode.Impulse);
 
-            if (Input.GetKeyDown(KeyCode.Space))
+             if (Input.GetKeyDown(KeyCode.Space) && noChão)
             {
-                rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+                rb.AddForce(Vector3.up * forcaPulo,ForceMode.Impulse);
+                noChão = false;
             }
-
-
+             
             if (transform.position.y <= -10)
             {
              //jogador caiu 

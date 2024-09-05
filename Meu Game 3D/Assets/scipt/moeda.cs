51f7084b047ed7112ed368cb0bba686805b31fba
ceciliaphr <cecilia.pinheiro@escolar.ifrn.edu.br>
2024-09-05 17:44:25 -0300

    Moeda

---
+++ Meu Game 3D/Assets/scipt/moeda.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moeda : MonoBehaviour
+{
+    public int velocidadeGiro = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OntriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(eulers: Vector3.up * velocidadeGiro * Time.deltaTime, relativeTo: Space.World);
+    }
+}
